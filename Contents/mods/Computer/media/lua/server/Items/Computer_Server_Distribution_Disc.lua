e53ff3e267a8b9fd83455f6fecf666db60d60ae7
Konijima <mathieugermain1991@gmail.com>
2021-12-24 00:01:53 -0500

    Augmented Distribution

--- Contents/mods/Computer/media/lua/server/Items/Computer_Server_Distribution_Disc.lua
+++ Contents/mods/Computer/media/lua/server/Items/Computer_Server_Distribution_Disc.lua
@@ -1,54 +1,50 @@
 require "Items/SuburbsDistributions"
 require "Items/ProceduralDistributions"
 
---- ProceduralDistributions
-
-local modifier = 1
-
 -- CrateCompactDiscs
 table.insert(ProceduralDistributions.list.CrateCompactDiscs.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.CrateCompactDiscs.items, 10 * modifier)
+table.insert(ProceduralDistributions.list.CrateCompactDiscs.items, 10)
 
 -- DeskGeneric
 table.insert(ProceduralDistributions.list.DeskGeneric.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.DeskGeneric.items, 5 * modifier)
+table.insert(ProceduralDistributions.list.DeskGeneric.items, 10)
 
 -- DresserGeneric
 table.insert(ProceduralDistributions.list.DresserGeneric.junk.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.DresserGeneric.junk.items, 2 * modifier)
+table.insert(ProceduralDistributions.list.DresserGeneric.junk.items, 10)
 
 -- LivingRoomShelf
 table.insert(ProceduralDistributions.list.LivingRoomShelf.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.LivingRoomShelf.items, 2 * modifier)
+table.insert(ProceduralDistributions.list.LivingRoomShelf.items, 10)
 
 -- LivingRoomShelfNoTapes
 table.insert(ProceduralDistributions.list.LivingRoomShelfNoTapes.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.LivingRoomShelfNoTapes.items, 2 * modifier)
+table.insert(ProceduralDistributions.list.LivingRoomShelfNoTapes.items, 10)
 
 -- Locker
 table.insert(ProceduralDistributions.list.Locker.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.Locker.items, 2 * modifier)
+table.insert(ProceduralDistributions.list.Locker.items, 10)
 
 -- LockerClassy
 table.insert(ProceduralDistributions.list.LockerClassy.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.LockerClassy.items, 2 * modifier)
+table.insert(ProceduralDistributions.list.LockerClassy.items, 10)
 
 -- SchoolLockers
 table.insert(ProceduralDistributions.list.SchoolLockers.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.SchoolLockers.items, 3 * modifier)
+table.insert(ProceduralDistributions.list.SchoolLockers.items, 10)
 
 -- ShelfGeneric
 table.insert(ProceduralDistributions.list.ShelfGeneric.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.ShelfGeneric.items, 2 * modifier)
+table.insert(ProceduralDistributions.list.ShelfGeneric.items, 10)
 
 -- WardrobeChild
 table.insert(ProceduralDistributions.list.WardrobeChild.junk.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.WardrobeChild.junk.items, 3 * modifier)
+table.insert(ProceduralDistributions.list.WardrobeChild.junk.items, 10)
 
 -- WardrobeMan
 table.insert(ProceduralDistributions.list.WardrobeMan.junk.items, "Computer.Disc_Game")
-table.insert(ProceduralDistributions.list.WardrobeMan.junk.items, 2 * modifier)
+table.insert(ProceduralDistributions.list.WardrobeMan.junk.items, 10)
 
 -- postbox
 table.insert(SuburbsDistributions.all.postbox.items, "Computer.Disc_Game")
-table.insert(SuburbsDistributions.all.postbox.items, 2 * modifier)
+table.insert(SuburbsDistributions.all.postbox.items, 10)
